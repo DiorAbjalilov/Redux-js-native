8a2142c42d029a323af98ced9239d9ccd31db640
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-02-23 12:44:25 +0500

    add

---
+++ asyncAction.js
@@ -0,0 +1,63 @@
+const redux=require('redux')
+const createStore=redux.createStore 
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require("redux-thunk").default
+const axios=require('axios')
+const initialState={
+    loading:false,
+    users:[],
+    error:''
+}
+
+const FETCH_USER_REQUEST = "FETCH_USER_REQUEST";
+const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS";
+const FETCH_USER_FAILURE = "FETCH_USER_FAILURE";
+
+const fetchUserRequest=()=>{
+    return{
+        type:FETCH_USER_REQUEST
+    }
+}
+const fetchUserSuccess=()=>{
+    return {
+        type:FETCH_USER_SUCCESS,
+        payload:users,
+    }
+}
+const fetchUserFailure=()=>{
+    return{
+        type:FETCH_USER_FAILURE,
+        payload:error
+    }
+}
+
+// 
+
+const reducer= (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USER_REQUEST:
+        return { 
+            ...state, 
+            loading:true 
+        };
+    case FETCH_USER_SUCCESS:
+        return { 
+          users:action.payload,
+          error:'',  
+        }
+    case FETCH_USER_REQUEST:
+        return { 
+            loading:false,
+            users:[],
+            error:action.payload 
+        }
+  }
+}
+
+const fetchUsers=()=>{
+    return function(dispatch){
+        axios.get("https://jsonplaceholder.typicode.com/users");
+    }
+}
+
+const store=createStore(reducer, applyMiddleware(thunkMiddleware))
